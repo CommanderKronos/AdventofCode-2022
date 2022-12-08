7bb89d1afac1ca199892a7f938444c5b3b9f7272
CommanderKronos <yuriwit0@gmail.com>
2022-12-08 23:36:59 +0100

    day 5 begin stack logic

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -33,15 +33,29 @@ impl<T> Stack<T> {
 }
 
 fn main() {
-    if let Ok(line_iter) = read_lines("./input.txt") {
+    if let Ok(line_iter) = read_lines("./stack.txt") {
         
         let lines: Vec<String> = line_iter.flat_map(|x| x).collect();
         // let lines: Vec<String> = line_iter.flat_map(|Ok(x)| x.clone()).collect();
         // Stack parsing
         // There will be 8 lines, with at most 9 elements in it (LESS IS POSSIBLE)
         // 9th and 10th lines need to be ignored. Maybe use stacks to split lines 1..=8 and 11..
+        let mut stack_vec: Vec<Stack<char>> = Vec::new();
+
         for line in lines {
-            
+            println!("{}", line);
+            println!("{:?}", line.as_bytes());
+            let mut temp_stack: Vec<char> = Vec::new();
+            let mut great_nr: u8 = 1;
+            for great in line.as_bytes().chunks(4) {
+                println!("{:?}", great);
+                if !(great == [32, 32, 32, 32]) {
+                    println!("Crate nr: {:?}", great_nr);
+                    println!("{:?}", great[1] as char);
+                    temp_stack.push(great[1] as char);
+                }
+                great_nr += 1;
+            }
         }
 
         // Stack manipulation
---
+++ day5/target/debug/deps/day5-0ebd4e0820a7f51d.d
@@ -0,0 +1,5 @@
+/home/kronos/scripts/AdventofCode-2022/day5/target/debug/deps/day5-0ebd4e0820a7f51d: src/main.rs
+
+/home/kronos/scripts/AdventofCode-2022/day5/target/debug/deps/day5-0ebd4e0820a7f51d.d: src/main.rs
+
+src/main.rs:
